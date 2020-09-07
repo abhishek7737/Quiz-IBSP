d11e4ac5732f25715b177f852e3e2a2bff28c3f8
unknown <abhishek.mathur1997@gmail.com>
2020-09-07 23:43:21 +0530

    first commit

--- index.php
+++ index.php
@@ -21,10 +21,10 @@
     <body>
         <center>
             <div class="intro">
-                <h1> SCAVENGER HUNT </h1>
+                <h1 style="color:yellow;"> <p style="font-family: Courier New">SCAVENGER HUNT </p> </h1>
                 <a href="login.php" class="btn"> login </a> &emsp;
                 <a href="register.php" class="btn"> register </a>
-                <h2> Good &nbsp;Luck. </h2>
+                <!--<h2> Get&nbsp;Ready &nbsp;To &nbsp; Hunt! </h2>-->
             </div>
         </center>
     </body>
